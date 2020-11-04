d62f850483969a52b9b69aa2cd0311bf90d4d6dc
Claudio Moreno <clauartur98@gmail.com>
2020-11-03 20:55:38 -0600

    feat: complete excercise day one

--- day_1.py
+++ day_1.py
@@ -3,3 +3,26 @@
 print('Day 1 - Python Print Function')
 print('The function is declared like this')
 print('What to print')
+
+# Look at the code in the code editor on the left. There are errors in all of the lines of code. Fix the code so that it runs without errors.
+# Warning: The output in your program should match the example output shown below exactly, character for character, even spaces and symbols should be identical, otherwise the tests won't pass.
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print("e.g. print(\"Hello\" + \"world\")")
+print("New lines can be created with a backslash and n.")
+
+
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+name = input("What's yor name ")
+print(len(name))
+
+#Write a program that switches the values stored in the variables a and b.
+a = input("a: ")
+b = input("b: ")
+
+
+#Project
+print("Hello")
+city = input("What's city wich you grew up ")
+name = input("What's the name of your pet ")
+print("Your band should be "+ city +" "+ name+"")
