0bf214c422d4066211b9cf83eea024d91aba1eaa
Claudio Moreno <clauartur98@gmail.com>
2020-11-18 14:39:58 -0600

    feat: add project day 5

--- day_5.py
+++ day_5.py
@@ -38,3 +38,40 @@ for score in student_scores:
   if score > max_score: 
     max_score = score
 print(max_score) 
+#########
+
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+total_size_string = nr_letters + nr_symbols + nr_numbers
+array_letter = []
+array_number = []
+array_symbols = []
+
+for letter in range(1, nr_letters + 1):
+  array_letter.append(random.choice(letters))
+
+
+for number in range(1, nr_numbers + 1):
+  array_number.append(random.choice(numbers))
+
+
+for symbol in range(1, nr_symbols + 1):
+  array_symbols.append(random.choice(symbols))
+
+final_array = array_letter + array_number + array_symbols
+#print(final_array)
+random.shuffle(final_array)
+#print(final_array)
+
+password = ""
+for letter in final_array:
+  password += letter
