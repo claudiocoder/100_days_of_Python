1afe4b37ecb84c45c5d84e1a3ac2f9712dad4f4d
Claudio Moreno <clauartur98@gmail.com>
2020-11-18 14:01:39 -0600

    feat: add new exercises with loops

--- day_5.py
+++ day_5.py
@@ -23,3 +23,18 @@ print(number_students)
 
 average = round(total_height / number_students)
 print(average)
+
+""" You are going to write a program that calculates the highest score from a List of scores.
+e.g. student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+Important you are not allowed to use the max or min functions. The output words must match the example. i.e
+The highest score in the class is: x """
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+max_score = 0
+for score in student_scores:
+  if score > max_score: 
+    max_score = score
+print(max_score) 
